c558446fe75d6bac87d54274a03faba3a948e292
unknown <gonzalosmoya24@gmail.com>
2023-03-11 02:48:23 +0100

    addded src/ folder

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "../libs/SDL/include/SDL2/SDL.h"
+
+int main(int argc, char* argv[]){
+	if(SDL_Init(SDL_INIT_VIDEO) <  0)
+	{
+		std::cout<<"SDL could not be initialized: "<<
+			SDL_GetError();
+	}else
+	{
+		std::cout<<"SDL video system is ready to go\n";
+	}
+
+
+	return 0;
+}
